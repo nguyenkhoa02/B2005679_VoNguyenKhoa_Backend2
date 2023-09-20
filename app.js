7c06ac9a4849060f161d6f409325efa521821df8
nguyenkhoa02 <nguyenkhoaht002@gmail.com>
2023-09-20 09:26:55 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const contactsRoute = require("./app/routes/contact.route");
 
+const  ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -13,5 +15,15 @@ app.get("/", (req, res) => {
 })
 
 app.use("/api/contacts", contactsRoute);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Interal Server Error",
+    });
+});
 
 module.exports = app;
